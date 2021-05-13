686632c4430c0a1b1429f4b228fa5b0668b7f658
Oli Evans <oli@tableflip.io>
2021-05-13 16:37:42 +0100

    feat: nice cli for --pack and --unpack
    - `--pack path/to/files` - pack files into a .car
    - `--unpack path/to/my.car` - unpack files from a .car
    - `--list path/to/my.car` - list files in a .car
    - `--list-cids path/to/my.car` - list cids in a .car
    
    and moved my api exploration to from-car.ts. it is imagined that we will have unpack functions in from-car.ts and packing functions in to-car.ts
    
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- src/cli.ts
+++ src/cli.ts
@@ -1,27 +1,91 @@
 #!/usr/bin/env node
 
-import fs from 'fs'
 import meow from 'meow'
-import { fromCarIterable } from './'
+import { unpackCarToFs, listFilesInCar, listCidsInCar } from './from-car.js'
 
 const options = {
   flags: {
     output: {
       type: 'string',
-      alias: 'o'
+      alias: 'o',
+    },
+    pack: {
+      type: 'string',
+      alias: 'p'
+    },
+    unpack: {
+      type: 'string',
+      alias: 'u',
+    },
+    list: {
+      type: 'string',
+      alias: 'l',
+    },
+    listCids: {
+      type: 'string'
     }
   }
 } as const;
 
 const cli = meow(`
-  Usage
-  $ ipfs-car <cid>
+  Content Addressable archives (CAR) store IPLD block data as a sequence of bytes;
+  typically in a file with a .car extension. The CAR format is a serialized 
+  representation of any IPLD DAG (graph) as the concatenation of its blocks, plus 
+  a header that describes the graphs in the file (via root CIDs).
+
+  See: https://github.com/ipld/specs/blob/master/block-layer/content-addressable-archives.md
+
+  --pack files into a .car
+  or
+  --unpack files from a .car
+  
+  Usage:
+    # pack files into a .car
+    $ ipfs-car --pack path/to/file/or/dir
+
+    # pack files into a car and specify the car file name
+    $ ipfs-car --pack path/to/files --output path/to/write/a.car
+
+    # unpack files from a .car 
+    $ ipfs-car --unpack path/to/my.car
+
+    # unpack files from a .car to a specific file path
+    $ ipfs-car --unpack path/to/my.car --output /path/to/unpack/files/to
+
+    # list cids in a .car
+    $ ipfs-car --list-cids path/to/my.ca
+
+    # list files in a .car
+    $ ipfs-car --list path/to/my.ca
+
 `, options)
 
-async function main () {
-  const inStream = fs.createReadStream(`${__dirname}/../test/fixtures/raw.car`)
-
-  await fromCarIterable(inStream)
+interface Flags {
+  output?: string,
+  pack?: string,
+  unpack?: string,
+  list?: string,
+  listCids?: string
 }
 
-main()
+async function handleInput ({flags}: { flags: Flags}) {
+  if (flags.pack) {
+    console.log('TODO! pack files into a .car')
+    // await fsToCar({input: flags.pack, output: flags.output})
+
+  } else if (flags.unpack) {
+    await unpackCarToFs({input: flags.unpack, output: flags.output})
+
+  } else if (flags.list) {
+    await listFilesInCar({input: flags.list})
+
+  } else if (flags.listCids) {
+    await listCidsInCar({input: flags.listCids})
+
+  } else {
+    cli.showHelp()
+    throw new Error('--pack or --unpack flag required')
+  }
+}
+
+handleInput(cli)
---
+++ src/from-car.ts
@@ -0,0 +1,108 @@
+// This is olizilla's fork of index.ts to show WIP.
+// It is imagined that we'd pull index.ts apart into from-car.ts and to-car.ts
+// so consumers could import just the direction they are interested in.
+
+import fs from 'fs'
+import pipe from 'it-pipe'
+import equals from 'uint8arrays/equals'
+import { map } from 'streaming-iterables'
+import BufferList from 'bl'
+
+import { CarIndexedReader, CarCIDIterator } from '@ipld/car'
+import { Block } from '@ipld/car/api'
+import { CID } from 'multiformats'
+import { sha256 } from 'multiformats/hashes/sha2'
+
+// appeasing tsc 
+interface CarReaderish {
+  getRoots(): Promise<CID[]>
+  get(key: CID): Promise<Block | undefined>
+}
+
+// appeasing tsc, figure out how to import from unix-fs-exporter
+interface UnixFSEntryish {
+  type: 'file' | 'directory' | 'object' | 'raw' | 'identity',
+  name: string
+  path: string
+  content: () => AsyncIterable<Uint8Array>
+}
+
+// TODO: Needs module with types commited + typedefinitions fixed (different cids...)
+// import exporter from 'ipfs-unixfs-exporter'
+const exporter: any = require('ipfs-unixfs-exporter')
+const toIterable = require('stream-to-it')
+
+// Node only, read a car from fs, write files to fs
+export async function unpackCarToFs ({input, output}: {input: string, output?: string}) {
+  const carReader = await CarIndexedReader.fromFile(input)
+  await writeFiles(fromCar(carReader), output)
+}
+
+export async function listFilesInCar ({input}: {input: string}) {
+  const carReader = await CarIndexedReader.fromFile(input)
+  for await (const file of fromCar(carReader)) {
+    console.log(file.path)
+  }
+}
+
+export async function listCidsInCar ({input}: {input: string}) {
+  const carIterator = await CarCIDIterator.fromIterable(fs.createReadStream(input))
+  for await (const cid of carIterator) {
+    console.log(cid.toString())
+  }
+}
+
+export async function* fromCar (carReader: CarReaderish, roots?: CID[] ) {
+  const verifyingBlockService = {
+    get: async (cid: CID) => {
+      const res = await carReader.get(cid)
+      if (!res) {
+        throw new Error(`Incomplete CAR. Block missing for CID ${cid}`)
+      }
+      if (!isValid(res)) {
+        throw new Error(`Invalid CAR. Hash of block data does not match CID ${cid}`)
+      }
+      return res
+    }
+  }
+
+  if (!roots) {
+    roots = await carReader.getRoots()
+  }
+
+  for (const root of roots) {
+    for await (const file of exporter.recursive(root, verifyingBlockService, { /* options */ })) {
+      yield file
+    }
+  }
+}
+
+export async function writeFiles (source: AsyncIterable<UnixFSEntryish>, output?: string) {
+  for await (const file of source) {
+    let filePath = file.path
+    // output overrides the first part of the path.
+    if (output) {
+      const parts = file.path.split('/')
+      parts[0] = output
+      filePath = parts.join('/')
+    }
+
+    if (file.type === 'file') {
+      await pipe(
+        file.content,
+        map((chunk: BufferList) => chunk.slice()), // BufferList to Buffer
+        toIterable.sink(fs.createWriteStream(filePath))
+      )
+    } else {
+      // this is a dir
+      await fs.promises.mkdir(filePath, { recursive: true })
+    }
+  }
+}
+
+async function isValid({ cid, bytes }: Block) {
+  // TODO: simple defaults for now
+  const hash = await sha256.digest(bytes)
+  return equals(hash.digest, cid.multihash.digest)
+}
+
