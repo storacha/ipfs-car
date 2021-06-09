adf95fa8e7a94a7a35d893a9ef2b3a5a583a146c
Vasco Santos <vasco.santos@moxy.studio>
2021-06-09 17:28:33 +0200

    chore: remove dist

--- dist/blockstore/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Block } from '@ipld/car/api';
-import { CID } from 'multiformats';
-export interface Blockstore {
-    put(block: Block): Promise<Block>;
-    get(cid: CID): Promise<Block>;
-    blocks(): AsyncGenerator<Block, void, unknown>;
-    destroy(): Promise<void>;
-}
--- dist/pack/fs.d.ts
+++
@@ -1,9 +0,0 @@
-import { Blockstore } from '../blockstore';
-export declare function packToFs({ input, output, blockstore: userBlockstore }: {
-    input: string | Iterable<string> | AsyncIterable<string>;
-    output?: string;
-    blockstore?: Blockstore;
-}): Promise<{
-    root: import("multiformats").CID;
-    filename: string;
-}>;
