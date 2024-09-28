e2c3e1041a4f0c1c9924adaf62f1c1b8569218d2
Oliver Kroener <ok@oliver-kroener.de>
2024-09-28 13:56:27 +0200

    [BUGFIX] Fixed issue with backslash on Windows. Was tested on web3.storage

--- cmd/pack.js
+++ cmd/pack.js
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import path from 'path'
+import os from 'os';
 import { Readable, Writable } from 'stream'
 import { filesFromPaths } from 'files-from-path'
 import { CarWriter } from '@ipld/car/writer'
@@ -19,7 +20,7 @@ const placeholderCID = CID.parse('bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3o
  * @param {boolean} [opts.hidden]
  * @param {string} [opts.output]
  */
-export default async function pack (filePath, opts = {}) {
+export default async function pack(filePath, opts = {}) {
   /* c8 ignore next */
   const rest = opts._ ?? []
   const paths = checkPathsExist([filePath, ...rest].filter(Boolean))
@@ -27,9 +28,19 @@ export default async function pack (filePath, opts = {}) {
   const files = paths.length
     ? await filesFromPaths(paths, { hidden })
     : /** @type {import('../types').FileLike[]} */ ([{ name: 'stdin', stream: () => Readable.toWeb(process.stdin) }])
+  // Check if the OS is Windows
+  const isWindows = os.platform() === 'win32';
+  // Normalize paths only if on Windows
+  const normalizedFiles = isWindows
+    ? files.map(file => ({
+      ...file,
+      name: file.name.replace(/\\/g, '/') // Replace \ with / only on Windows
+    }))
+    : files;
+
   const blockStream = files.length === 1 && (files[0].name === 'stdin' || !opts.wrap)
-    ? UnixFS.createFileEncoderStream(files[0])
-    : UnixFS.createDirectoryEncoderStream(files)
+    ? UnixFS.createFileEncoderStream(normalizedFiles[0])
+    : UnixFS.createDirectoryEncoderStream(normalizedFiles)
   const carEncoderStream = new CAREncoderStream(opts.output ? [placeholderCID] : [])
   const outStream = Writable.toWeb(opts.output ? fs.createWriteStream(opts.output) : process.stdout)
   await blockStream.pipeThrough(carEncoderStream).pipeTo(outStream)
@@ -46,7 +57,7 @@ export default async function pack (filePath, opts = {}) {
 }
 
 /** @param {string[]} paths */
-function checkPathsExist (paths) {
+function checkPathsExist(paths) {
   for (const p of paths) {
     if (!fs.existsSync(p)) {
       console.error(`The path ${path.resolve(p)} does not exist`)
