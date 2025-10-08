39fc03f0150c7f933caf39b425728de05d4f66e2
ash <alan@storacha.network>
2025-10-08 12:24:33 +0100

    feat(cli): hash can output a base58btc encoded multihash (#176)

--- cmd/hash.js
+++ cmd/hash.js
@@ -4,12 +4,16 @@ import { pipeline } from 'stream/promises'
 import { CID } from 'multiformats/cid'
 import * as Digest from 'multiformats/hashes/digest'
 import { sha256 } from 'multiformats/hashes/sha2'
+import { base58btc } from 'multiformats/bases/base58'
 
 /** CAR CID code */
 const carCode = 0x0202
 
-/** @param {string} carPath */
-export default async function hash (carPath) {
+/**
+ * @param {string} carPath
+ * @param {{ 'only-multihash': boolean }} [opts]
+ */
+export default async function hash (carPath, opts) {
   const hasher = crypto.createHash('sha256')
 
   await pipeline(
@@ -22,5 +26,9 @@ export default async function hash (carPath) {
   )
 
   const digest = Digest.create(sha256.code, hasher.digest())
-  console.log(CID.createV1(carCode, digest).toString())
+  if (opts?.['only-multihash']) {
+    console.log(base58btc.encode(digest.bytes))
+  } else {
+    console.log(CID.createV1(carCode, digest).toString())
+  }
 }
